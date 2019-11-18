4069d5b4b1b59b9cf3660ec25bbed04bc209f06b
rbenninger <rbenninger@rogers.com>
2019-11-18 13:09:49 -0500

    added functions

--- main.c
+++ main.c
@@ -1,6 +1,102 @@
+bool OpenFile(istream fin)
+{
+	
+}
 
+bool OnPaper()
+{
+	
+}
 
+double SpaceLeft()
+{
+	
+}
 
+double GetWidth(string word)
+{
+	
+}
+
+void AddSmall()
+{
+	
+}
+
+void AddLarge()
+{
+	
+}
+
+bool NotSkew()
+{
+	
+}
+
+void MovePen(double x, double y)
+{
+	
+}
+
+void PauseTimer(int current)
+{
+	
+}
+
+void PenUp()
+{
+	
+}
+
+void PenDown()
+{
+	
+}
+
+void PressEnter()
+{
+	
+}
+
+void ResetArm()
+{
+	
+}
+
+void NewLine()
+{
+	
+}
+
+void RefillPaper()
+{
+	
+}
+
+bool StartUp()
+{
+	
+}
+
+void ShutDown()
+{
+	
+}
+
+void WriteLetter(char letter)
+{
+	
+}
+
+bool TooLong(string word)
+{
+	
+}
+
+int WriteWord(string word)
+{
+	
+}
 
 task main()
 {
